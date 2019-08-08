b36731a01cdf1180e1cfdf59ac19dc37a96c3eb4
Pavel Ershov <pershov@game-insight.com>
2019-08-08 11:06:25 +0300

    Another commentaries

--- grokking_algorithms/04_quicksort/golang/05_quicksort.go
+++ grokking_algorithms/04_quicksort/golang/05_quicksort.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func quicksort(list []int) []int {
-	if len(list) < 2 {
-		return list
-	} else {
+	// Сортировать имеет только большие массивы
+	if len(list) >= 2 {
+		// Выбираем опорный элемент
 		pivot := list[0]
 
+		// Создаем списки для всех элементов больше и всех элементов меньше опорного
 		var less = []int{}
 		var greater = []int{}
+
+		// Идем по списку, по всем элементам кроме опорного
 		for _, num := range list[1:] {
+			// Добавляем в нужные массивы
 			if pivot > num {
 				less = append(less, num)
 			} else {
@@ -18,9 +22,16 @@ func quicksort(list []int) []int {
 			}
 		}
 
-		less = append(quicksort(less), pivot)
+		// Рекурсивно сортируем элементы меньше текущего
+		less = quicksort(less)
+		// Добавляем опорный элемент
+		less = append(less, pivot)
+		// Сортируем элементы больше текущего
 		greater = quicksort(greater)
+		// Финальный массив
 		return append(less, greater...)
+	} else {
+		return list
 	}
 }
 
