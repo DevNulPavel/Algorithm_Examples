90ecaf76b0cc816e396b007e668f0d5aa975f87b
Pavel Ershov <pershov@game-insight.com>
2019-08-12 13:02:53 +0300

    New commentaries

--- grokking_algorithms/07_dijkstras_algorithm/python/01_dijkstras_algorithm.py
+++ grokking_algorithms/07_dijkstras_algorithm/python/01_dijkstras_algorithm.py
@@ -1,66 +1,103 @@
-# the graph
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+infinity = float("inf")
+
+# Описание графа
 graph = {}
-graph["start"] = {}
-graph["start"]["a"] = 6
-graph["start"]["b"] = 2
-
-graph["a"] = {}
-graph["a"]["fin"] = 1
-
+graph["start"] = {"a": 6, "b": 2 }
+graph["a"] = {"fin": 1}
 graph["b"] = {}
-graph["b"]["a"] = 3
-graph["b"]["fin"] = 5
-
+graph["b"] = {"a": 3, "fin": 5}
 graph["fin"] = {}
 
-# the costs table
-infinity = float("inf")
+# Таблица стоимостей перехода к конкретному узлу
 costs = {}
 costs["a"] = 6
 costs["b"] = 2
 costs["fin"] = infinity
 
-# the parents table
+# Таблица переходов от чилда к родителю, нужна для восстановления обратного пути от конца в начало
 parents = {}
 parents["a"] = "start"
 parents["b"] = "start"
 parents["fin"] = None
 
-processed = []
+# Список уже обработанных нодов
+processed = set()
 
+# Конвертируем словарь родителей в путь от конечного нода до начального
+def convert_parents_to_path(parents, start_node, end_node):
+    result = []
+
+    total_weight = 0
+
+    parent = end_node
+    result.append(parent)
+
+    complete = False
+    while not complete:
+        parent = parents[parent]
+        result.append(parent)
+
+        if parent == start_node:
+            complete = True
+
+    result.reverse()
+
+    return result
+
+# Функция поиска ближайшего по стоимости нода с учетом уже обработанных
 def find_lowest_cost_node(costs):
+    # Значение наименьшей стоимости
     lowest_cost = float("inf")
     lowest_cost_node = None
-    # Go through each node.
+
+    # Обходим все ноды и ищем нод с наименьшей стоимостью и не обработанный
     for node in costs:
         cost = costs[node]
-        # If it's the lowest cost so far and hasn't been processed yet...
-        if cost < lowest_cost and node not in processed:
-            # ... set it as the new lowest-cost node.
+        # Если нод с наименьшей стоиместью, чем последний + не был обработан
+        if (cost < lowest_cost) and (node not in processed):
+            # Обновляем значения
             lowest_cost = cost
             lowest_cost_node = node
+    # Возвращаем полученное значение
     return lowest_cost_node
 
-# Find the lowest-cost node that you haven't processed yet.
-node = find_lowest_cost_node(costs)
-# If you've processed all the nodes, this while loop is done.
-while node is not None:
-    cost = costs[node]
-    # Go through all the neighbors of this node.
-    neighbors = graph[node]
-    for n in neighbors.keys():
-        new_cost = cost + neighbors[n]
-        # If it's cheaper to get to this neighbor by going through this node...
-        if costs[n] > new_cost:
-            # ... update the cost for this node.
-            costs[n] = new_cost
-            # This node becomes the new parent for this neighbor.
-            parents[n] = node
-    # Mark the node as processed.
-    processed.append(node)
-    # Find the next node to process, and loop.
+def find_lowest_path():
+    # Ищем нод с наименьшей стоимостью из необработанных
     node = find_lowest_cost_node(costs)
 
-print("Cost from the start to each node:")
-print(costs)
+    # Если такой нод нашелся - начинаем поиск
+    while node is not None:
+        # Получаем текущую стоимость перехода к данному ноду (включая косвенный переход)
+        cost = costs[node]
+
+        # Получаем список соседей данного нода
+        neighbors = graph[node]
+
+        # Перебираем список соседей текущего нода
+        for n in neighbors.keys():
+            # Вычисляем новую суммарную стоимость перехода к текущему ноду
+            new_cost = cost + neighbors[n]
+            # Если предыдущая стоимость перехода к текущему ноду выше, чем текущая новая,
+            # то имеет смысл обновить новую стоимость перехода
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                # так как переход дешевле, то обновляем родителя для обратного разворачивания
+                parents[n] = node
+        # Помечаем нод как обработанный
+        processed.add(node)
+        # Заново ищем нод с наименьшей стоимостью, который мы еще не обработали
+        node = find_lowest_cost_node(costs)
+
+    print("Cost from the start to each node:")
+    print(costs)
+    path = convert_parents_to_path(parents, "start", "fin")
+    print path
+    print("Total path weight: " + str(costs["fin"]))
+
+
+find_lowest_path();
 
